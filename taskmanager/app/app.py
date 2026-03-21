276e35769ddb2c824d8bd42007ab76851e4e6110
VaishakRR <vaishakrr97@gmail.com>
2026-03-21 10:47:55 +0000

    added new files taskmanager/app/.env,  taskmanager/docker-compose.yml and modified taskmanager/app/app.py and taskmanager/app/requirements.txt ran a multi container app as a single unit with Docker Compose: Flask + PostgreSQL + Redis

--- taskmanager/app/app.py
+++ taskmanager/app/app.py
@@ -1,16 +1,60 @@
 #!/usr/bin/python3
-
+import os
+import redis
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# postgresql connection
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+        'DATABASE_URI',
+        'postgresql://taskuser:taskpass@db:5432/taskdb'
+)
+
+db = SQLAlchemy(app)
+
+# Redis connection
+cache = redis.Redis(
+        host=os.getenv('REDIS_HOST', 'redis'),
+        port=6379,
+        decode_responses=True
+)
+
+# Task model
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    done = db.Column(db.Boolean, default=False)
 
 @app.route('/')
 def home():
     return jsonify({"message": "Task Manager API is running"})
 
 @app.route('/tasks')
-def tasks():
-    return jsonify({"tasks": ["Buy groceries", "Write Dockerfile", "Learn Docker"]})
+def get_tasks():
+    cached = cache.get('tasks')
+    if cached:
+        return jsonify({"source": "cache", "tasks": cached})
+    tasks = Task.query.all()
+    result = [{"id": t.id, "title": t.title, "done": t.done} for t in tasks]
+
+    cache.setex('tasks', 30, str(result))
+    return jsonify({"source": "database", "tasks": result})
+
+@app.route('/tasks/seed')
+def seed():
+    with app.app_context():
+        db.create_all() 
+        if Task.query.count() == 0:
+            sample = [Task(title="Buy groceries"), Task(title="Write Dockerfile"), Task(title="Learn Docker")]
+            db.session.add_all(sample)
+            db.session.commit()
+    return jsonify({"message": "Database seeded!"})
 
 if __name__=='__main__':
+    with app.app_context():
+        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+
