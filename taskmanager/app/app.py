c68ae00661711837d8511f7c3e703df4c7ebb370
VaishakRR <vaishakrr97@gmail.com>
2026-03-25 18:07:45 +0000

    wrote production and development differencing .yml files for compartmentalization

--- taskmanager/app/app.py
+++ taskmanager/app/app.py
@@ -55,6 +55,7 @@ def seed():
 if __name__=='__main__':
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    is_dev = os.getenv('FLASK_ENV') == 'development'
+    app.run(host='0.0.0.0', port=5000, debug=is_dev)
 
 
